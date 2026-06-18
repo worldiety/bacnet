79a57fc557203fae41b0d8e4c4e5067af410b453
Lukas Meier <lukas.meier@worldiety.de>
2026-06-18 15:32:58 +0200

    docs: updated example

--- common/log/logging.go
+++ common/log/logging.go
@@ -5,7 +5,11 @@ import (
 	"os"
 )
 
-var Logger *slog.Logger = slog.New(slog.NewTextHandler(nil, nil))
+var Logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+	AddSource:   false,
+	Level:       slog.LevelError,
+	ReplaceAttr: nil,
+}))
 
 func InitLogger(level slog.Level, addSource bool) {
 	Logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
--- examples/discover/main.go
+++ examples/discover/main.go
@@ -4,22 +4,28 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/netip"
+	"os"
 	"time"
 
 	"go.wdy.de/bacnet"
 	"go.wdy.de/bacnet/apdu"
+	baclog "go.wdy.de/bacnet/common/log"
 	"go.wdy.de/bacnet/common/netprim"
 )
 
 func main() {
-	err := runDiscover()
+	args := os.Args[1:]
+	err := runDiscover(args)
 	if err != nil {
 		log.Fatal("discover failed", err)
 	}
 }
 
-func runDiscover() error {
+func runDiscover(args []string) error {
+	baclog.InitLogger(slog.LevelDebug, false)
+
 	rt, err := bacnet.NewClientRuntime(netip.AddrFrom4([4]byte{0, 0, 0, 0}), bacnet.ClientRuntimeConfig{
 		MaxDatagramSize: 0,
 		ASE:             apdu.DefaultASEConfig(),
@@ -28,7 +34,6 @@ func runDiscover() error {
 	if err != nil {
 		return err
 	}
-	defer rt.Close()
 
 	go func() {
 		_ = rt.Run(context.Background())
@@ -37,10 +42,10 @@ func runDiscover() error {
 	devices, err := rt.Client().Discover(context.Background(), apdu.DiscoverRequest{
 		Destination: netprim.Address{
 			Network:  netprim.LocalNetwork,
-			AddrPort: netip.AddrPortFrom(netip.MustParseAddr("192.168.8.255"), 47808),
+			AddrPort: netip.AddrPortFrom(netip.MustParseAddr(args[0]), 47808),
 		},
 		WhoIs:  apdu.NewWhoIsRequest(),
-		Window: time.Second * 20,
+		Window: time.Second * 10,
 	})
 	if err != nil {
 		return err
@@ -48,6 +53,10 @@ func runDiscover() error {
 
 	for _, device := range devices {
 		fmt.Println(device)
+	}
+
+	if len(devices) == 0 {
+		fmt.Println("No devices")
 	}
 
 	return nil
